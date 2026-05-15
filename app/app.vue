2f11704b7bc03cec96bbc5dfe704b2d6a91c0549
Linus Schärmann <linus.schaermann@managrsphere.com>
2026-05-15 12:34:49 +0200

    Favicon as public asset

--- app/app.vue
+++ app/app.vue
@@ -3,13 +3,15 @@ const colorMode = useColorMode()
 
 const color = computed(() => colorMode.value === 'dark' ? '#09090b' : 'white')
 
+const publicAsset = usePublicAsset()
+
 useHead({
   meta: [
     { name: 'viewport', content: 'width=device-width, initial-scale=1' },
     { key: 'theme-color', name: 'theme-color', content: color }
   ],
   link: [
-    { rel: 'icon', href: '/favicon.ico' }
+    { rel: 'icon', href: publicAsset('/favicon.ico') }
   ],
   htmlAttrs: {
     lang: 'de'
@@ -17,7 +19,7 @@ useHead({
 })
 
 useSeoMeta({
-  ogImage: '/IRCP_Logo.png',
+  ogImage: publicAsset('/IRCP_Logo.png'),
   twitterCard: 'summary_large_image'
 })
 </script>
